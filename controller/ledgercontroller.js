109c4fa08e0c44ef9d606e67b13751eac36400d6
murugeswari <murugeswarivellilingam@gmail.com>
2023-10-12 09:22:13 +0530

    checking

--- controller/ledgercontroller.js
+++ controller/ledgercontroller.js
@@ -2,5 +2,5 @@ const LedgerModel=require("../models/ledgerModel");
 
 module.exports.getLeger=async(req,res)=>{
     const ledger=await LedgerModel.find()
-    res.send("muru")
+    res.send(ledger)
 }
