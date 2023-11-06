d85da67acbda83abe5ea54be3501f0046afd2c33
murugeswari <murugeswarivellilingam@gmail.com>
2023-11-06 08:56:14 +0530

    Total Ledger

--- controller/cityNamecontroller.js
+++ controller/cityNamecontroller.js
@@ -1,4 +1,6 @@
 const cityNameModel = require("../models/cityNameModel")
+const lineModel = require("../models/lineModel")
+const loanModel = require("../models/loanModel")
 
 //all city
 module.exports.getCityNames=async(req,res)=>{
@@ -82,4 +84,353 @@ module.exports.deleteCityNames=(req,res)=>{
         console.log(err);
         res.send({error:err,msg:"somthing went wrong"})
     })
+}
+
+
+//Total Ledger Report//
+module.exports.totalLedger=(req,res)=>{
+ const Ledger=loanModel.aggregate(
+  [
+    {
+      '$lookup': {
+        'from': 'loantables', 
+        'let': {
+          'lineno': '$lineno'
+        }, 
+        'pipeline': [
+          {
+            '$match': {
+              '$expr': {
+                '$and': [
+                  {
+                    '$eq': [
+                      '$lineno', '$$lineno'
+                    ]
+                  }, {
+                    '$lte': [
+                      '$startdate', new Date('Mon, 06 Nov 2023 00:00:00 GMT')
+                    ]
+                  }
+                ]
+              }
+            }
+          }, {
+            '$group': {
+              '_id': {
+                'lineno': '$lineno', 
+                'lineman_id': '$lineman_id'
+              }, 
+              'totalamountbefore': {
+                '$sum': '$totalamount'
+              }, 
+              'countbefore': {
+                '$sum': 1
+              }
+            }
+          }
+        ], 
+        'as': 'loansub'
+      }
+    }, {
+      '$unwind': {
+        'path': '$loansub', 
+        'includeArrayIndex': 'string', 
+        'preserveNullAndEmptyArrays': true
+      }
+    }, {
+      '$lookup': {
+        'from': 'receipttables', 
+        'let': {
+          'loannumber': '$loannumber'
+        }, 
+        'pipeline': [
+          {
+            '$match': {
+              '$expr': {
+                '$and': [
+                  {
+                    '$eq': [
+                      '$loannumber', '$$loannumber'
+                    ]
+                  }, {
+                    '$lte': [
+                      '$receiptdate', new Date('Mon, 06 Nov 2023 00:00:00 GMT')
+                    ]
+                  }
+                ]
+              }
+            }
+          }, {
+            '$group': {
+              '_id': '$loannumber', 
+              'collectedbefore': {
+                '$sum': '$collectedamount'
+              }
+            }
+          }
+        ], 
+        'as': 'receiptbefore'
+      }
+    }, {
+      '$unwind': {
+        'path': '$receiptbefore', 
+        'includeArrayIndex': 'string', 
+        'preserveNullAndEmptyArrays': true
+      }
+    }, {
+      '$lookup': {
+        'from': 'receipttables', 
+        'let': {
+          'loannumber': '$loannumber'
+        }, 
+        'pipeline': [
+          {
+            '$match': {
+              '$expr': {
+                '$and': [
+                  {
+                    '$eq': [
+                      '$loannumber', '$$loannumber'
+                    ]
+                  }, {
+                    '$lte': [
+                      '$receiptdate', new Date('Mon, 13 Nov 2023 00:00:00 GMT')
+                    ]
+                  }
+                ]
+              }
+            }
+          }, {
+            '$group': {
+              '_id': '$loannumber', 
+              'collectedafter': {
+                '$sum': '$collectedamount'
+              }
+            }
+          }
+        ], 
+        'as': 'receiptafter'
+      }
+    }, {
+      '$lookup': {
+        'from': 'receipttables', 
+        'let': {
+          'loannumber': '$loannumber'
+        }, 
+        'pipeline': [
+          {
+            '$match': {
+              '$expr': {
+                '$and': [
+                  {
+                    '$eq': [
+                      '$loannumber', '$$loannumber'
+                    ]
+                  }, {
+                    '$lte': [
+                      '$receiptdate', new Date('Mon, 13 Nov 2023 00:00:00 GMT')
+                    ]
+                  }, {
+                    '$gte': [
+                      '$receiptdate', new Date('Mon, 06 Nov 2023 00:00:00 GMT')
+                    ]
+                  }
+                ]
+              }
+            }
+          }, {
+            '$group': {
+              '_id': '$loannumber', 
+              'collectedbetween': {
+                '$sum': '$collectedamount'
+              }
+            }
+          }
+        ], 
+        'as': 'receiptbetween'
+      }
+    }, {
+      '$unwind': {
+        'path': '$receiptafter', 
+        'includeArrayIndex': 'string', 
+        'preserveNullAndEmptyArrays': true
+      }
+    }, {
+      '$unwind': {
+        'path': '$receiptbetween', 
+        'includeArrayIndex': 'string', 
+        'preserveNullAndEmptyArrays': true
+      }
+    }, {
+      '$project': {
+        'loannumber': 1, 
+        'lineman_id': 1, 
+        'lineno': 1, 
+        'totalamount': 1, 
+        'dueamount': 1, 
+        'collectedamountbefore': {
+          '$ifNull': [
+            '$receiptbefore.collectedbefore', 0
+          ]
+        }, 
+        'collectedamountafter': {
+          '$ifNull': [
+            '$receiptafter.collectedafter', 0
+          ]
+        }, 
+        'collectedamountbetween': {
+          '$ifNull': [
+            '$receiptbetween.collectedamountbetween', 0
+          ]
+        }, 
+        'weekcount': 1, 
+        'addFields': {
+          'daysCount': {
+            '$add': [
+              {
+                '$round': {
+                  '$divide': [
+                    {
+                      '$subtract': [
+                        new Date('Mon, 06 Nov 2023 00:00:00 GMT'), '$startdate'
+                      ]
+                    }, 86400000 * 7
+                  ]
+                }
+              }, 1
+            ]
+          }, 
+          'daysCountafter': {
+            '$add': [
+              {
+                '$round': {
+                  '$divide': [
+                    {
+                      '$subtract': [
+                        new Date('Mon, 13 Nov 2023 00:00:00 GMT'), '$startdate'
+                      ]
+                    }, 86400000 * 7
+                  ]
+                }
+              }, 1
+            ]
+          }
+        }, 
+        'totalamountbefore': {
+          '$ifNull': [
+            '$loansub.totalamountbefore', 0
+          ]
+        }, 
+        'countbefore': '$loansub.countbefore'
+      }
+    }, {
+      '$project': {
+        'loannumber': 1, 
+        'lineman_id': 1, 
+        'lineno': 1, 
+        'totalamount': 1, 
+        'dueamount': 1, 
+        'collectedamountbetween': 1, 
+        'totalamountbefore': 1, 
+        'countbefore': 1, 
+        'pendingamountbefore': {
+          '$subtract': [
+            {
+              '$multiply': [
+                '$dueamount', '$addFields.daysCount'
+              ]
+            }, '$collectedamountbefore'
+          ]
+        }, 
+        'pendingamountafter': {
+          '$subtract': [
+            {
+              '$multiply': [
+                '$dueamount', '$addFields.daysCountafter'
+              ]
+            }, '$collectedamountafter'
+          ]
+        }
+      }
+    }, {
+      '$group': {
+        '_id': {
+          'lineno': '$lineno', 
+          'lineman_id': '$lineman_id', 
+          'totalamountbefore': '$totalamountbefore', 
+          'countbefore': '$countbefore'
+        }, 
+        'pendingbefore': {
+          '$sum': '$pendingamountbefore'
+        }, 
+        'pendingafter': {
+          '$sum': '$pendingamountafter'
+        }, 
+        'totalafter': {
+          '$sum': '$totalamount'
+        }, 
+        'collectedbetween': {
+          '$sum': '$collectedamountbetween'
+        }, 
+        'countafter': {
+          '$sum': 1
+        }
+      }
+    }, {
+      '$replaceRoot': {
+        'newRoot': {
+          '$mergeObjects': [
+            {
+              'lineno': '$_id.lineno', 
+              'lineman_id': '$_id.lineman_id', 
+              'totalamountbefore': '$_id.totalamountbefore', 
+              'countbefore': '$_id.countbefore'
+            }, '$$ROOT'
+          ]
+        }
+      }
+    }, {
+      '$lookup': {
+        'from': 'linetables', 
+        'localField': 'lineno', 
+        'foreignField': 'lineno', 
+        'as': 'linesub'
+      }
+    }, {
+      '$unwind': {
+        'path': '$linesub', 
+        'includeArrayIndex': 'string', 
+        'preserveNullAndEmptyArrays': true
+      }
+    }, {
+      '$lookup': {
+        'from': 'linemantables', 
+        'localField': 'lineman_id', 
+        'foreignField': '_id', 
+        'as': 'linemansub'
+      }
+    }, {
+      '$unwind': {
+        'path': '$linemansub', 
+        'includeArrayIndex': 'string', 
+        'preserveNullAndEmptyArrays': true
+      }
+    }, {
+      '$project': {
+        'lineno': 1, 
+        'lineman_id': 1, 
+        'totalamountbefore': 1, 
+        'countbefore': 1, 
+        'totalamountafter': 1, 
+        'countafter': 1, 
+        'pendingbefore': 1, 
+        'pendingafter': 1, 
+        'collectedbetween': 1, 
+        'linename': '$linesub.linename', 
+        'linemanname': '$linemansub.linemanname'
+      }
+    }
+  ]
+ ) 
+ res.send(Ledger);
 }
--- routes/citynameroute.js
+++ routes/citynameroute.js
@@ -1,10 +1,14 @@
 const express=require('express')
 const router=express.Router()
-const{getCityNames,deleteCityNames,updateCityNames,saveCityNames,getMaxCityCode}=require("../controller/cityNamecontroller");
+const{getCityNames,deleteCityNames,updateCityNames,saveCityNames,totalLedger}=require("../controller/cityNamecontroller");
 router.get("/citycreate/get",getCityNames);
 router.post("/citycreate/save",saveCityNames);
 router.put("/citycreate/update/:id",updateCityNames);
 router.delete("/delete/:id",deleteCityNames)
+
+//Total Ledger Section//
+router.get("/city/totalledger",totalLedger);
+
 //router.get("/citycreate/get/max",getMaxCityCode);
 
 module.exports=router
