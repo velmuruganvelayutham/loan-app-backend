93e53ba9856d474e43bd2ce797716692446c9b0d
murugeswari <murugeswarivellilingam@gmail.com>
2023-12-27 18:16:06 +0530

    receipt changed

--- controller/customercontroller.js
+++ controller/customercontroller.js
@@ -43,7 +43,7 @@ module.exports.getCustomerWithCity = async (req, res) => {
     },
     {
       '$sort': {
-        '_id':1,'customer': 1,'mobileno':1,'cityname':1
+        '_id': 1, 'customer': 1, 'mobileno': 1, 'cityname': 1
       }
     }
   ]);
@@ -94,18 +94,18 @@ module.exports.deleteCustomer = (req, res) => {
       res.send({ error: err, msg: "somthing went wrong" })
     })*/
   customerModel.findByIdAndDelete(id)
-  .then(()=> res.send("Deleted Successfully"))
-  .catch((err)=>{
+    .then(() => res.send("Deleted Successfully"))
+    .catch((err) => {
       console.log(err);
-      res.send({error:err,msg:"somthing went wrong"})
-  })
+      res.send({ error: err, msg: "somthing went wrong" })
+    })
 }
 
 //Receipt Section
 
 //get receiptdetails//
-module.exports.getReceipt=async(req,res)=>{
-  const receipt = await receiptModel.find({'loannumber':req.query['loannumber']})
+module.exports.getReceipt = async (req, res) => {
+  const receipt = await receiptModel.find({ 'loannumber': req.query['loannumber'] })
   res.send(receipt)
 }
 
@@ -204,6 +204,172 @@ module.exports.getPendingLoan = async (req, res) => {
   res.send(resview);
 }
 
+module.exports.getPendingLoanDuplicate = async (req, res) => {
+  const loanno = req.query['loanno'];
+  //const resview=await pendingloanModel.find();
+  const resview = await pendingloanModel.aggregate(
+    [
+      {
+        $lookup: {
+          from: 'receipttables',
+          localField: 'loannumber',
+          foreignField: 'loannumber',
+          as: 'joined'
+        },
+      },
+
+      {
+        $match: {
+          "joined.receiptdate": { $not: { $eq: new Date(req.query['receiptdate']) } }
+        }
+      }
+      ,
+      {
+        $unwind: {
+          path: '$joined',
+          includeArrayIndex: 'string',
+          preserveNullAndEmptyArrays: true
+        }
+      },
+      {
+        $group: {
+          _id: {
+            loannumber: '$loannumber',
+            customer_id: '$customer_id',
+            totalamount: '$totalamount',
+            customer: "$customer",
+            dueamount: "$dueamount",
+            city: "$city",
+            cityid: "$cityid"
+          },
+          collected: { $sum: '$joined.collectedamount' }
+        }
+      },
+
+      {
+        $project: {
+          loannumber: 1,
+          customer_id: 1,
+          customer: 1,
+          totalamount: 1,
+          dueamount: 1,
+          city: 1,
+          collected: 1,
+          cityid: 1,
+          "pending": { $subtract: ["$_id.totalamount", "$collected"] }
+        }
+      },
+
+      {
+        $replaceRoot: {
+          newRoot: {
+            $mergeObjects: [
+              {
+                customer: '',
+                loannumber: '$_id.loannumber',
+                totalamount: '$_id.totalamount',
+                dueamount: '$_id.dueamount',
+                city: '$_id.city',
+                cityid: '$_id.cityid',
+                check: false,
+                amount: 0,
+                weak: 0
+              },
+              '$$ROOT'
+            ]
+          }
+        }
+      },
+
+      {
+        $match: {
+          $and: [
+            { loannumer: { $eq: loanno } },
+            { pending: { $gt: 0 } }
+          ]
+        }
+      }
+
+    ]);
+  res.send(resview);
+}
+//get receipt details to edit///
+module.exports.getReceiptDetails = async (req, res) => {
+  const cityid = req.query['cityid'];
+  const receiptde = await receiptModel.aggregate([
+    {
+      '$lookup': {
+        'from': 'customertables',
+        'localField': 'customer_id',
+        'foreignField': '_id',
+        'as': 'joined'
+      }
+    }, {
+      '$unwind': {
+        'path': '$joined',
+        'includeArrayIndex': 'string',
+        'preserveNullAndEmptyArrays': True
+      }
+    }, {
+      '$project': {
+        'loannumber': 1,
+        'receiptdate': 1,
+        'weekno': 1,
+        'customer_id': 1,
+        'customer': '$joined.customer',
+        'collectedamount': 1,
+        'addFields': {
+          'check': {
+            '$cond': {
+              'if': {
+                '$eq': [
+                  0, 1
+                ]
+              },
+              'then': True,
+              'else': False
+            }
+          },
+          'amount': {
+            '$cond': {
+              'if': {
+                '$eq': [
+                  1, 0
+                ]
+              },
+              'then': 1,
+              'else': 0
+            }
+          }
+        },
+        'cityid': {
+          '$toString': '$joined.city'
+        },
+        'weak': '$weekno'
+      }
+    }, {
+      '$match': {
+        '$and': [
+          {
+            'cityid': {
+              '$eq': cityid
+            }
+          }, {
+            'receiptdate': {
+              '$eq': new Date(req.query['receiptdate'])
+            }
+          }
+        ]
+      }
+    }, {
+      '$sort': {
+        'loannumber': 1
+      }
+    }
+
+  ])
+  res.send(receiptde)
+}
 module.exports.saveReceipt = (req, res) => {
   let items = req.body.receiptdata.map(item => {
     if (item.check) {
@@ -228,8 +394,8 @@ module.exports.saveReceipt = (req, res) => {
 }
 
 ///All Loan//
-module.exports.getLoanDetails=async(req,res)=>{
-  const loan=await loanPendingModelView.find()
+module.exports.getLoanDetails = async (req, res) => {
+  const loan = await loanPendingModelView.find()
   res.send(loan);
 }
 
--- routes/customerroute.js
+++ routes/customerroute.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const router=express.Router()
 const {getCustomers,saveCustomer,updateCustomer,deleteCustomer, getCustomerWithCity,
-    getPendingLoan,saveReceipt,getLoanDetails,getReceipt}=require("../controller/customercontroller")
+    getPendingLoan,saveReceipt,getLoanDetails,getReceipt,getReceiptDetails,getPendingLoanDuplicate}=require("../controller/customercontroller")
 router.get("/get",getCustomers);
 router.get("/get/view",getCustomerWithCity)
 router.post("/save",saveCustomer)
@@ -9,6 +9,7 @@ router.put("/update/:id",updateCustomer)
 router.delete("/delete/:id",deleteCustomer)
 //receipt section
 router.get("/receipt/get/loanpending",getPendingLoan);
+router.get("/receipt/get/loanpendingDuplicate",getPendingLoanDuplicate);
 router.post("/receipt/save/details",saveReceipt);
 router.get("/receipt/get",getReceipt);
 //loan details//
